d5c668bd6db9c8c7252752062cf01d14cd8f0100
Hank Hester <hankhester1@gmail.com>
2020-07-31 17:22:18 -0700

    Set up speech sdk

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Threading.Tasks;
+
+namespace diy_smart_home
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var keywordModel = KeywordRecognitionModel.FromFile("./Hey_Roku.table");
+            var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            var keywordRecognizer = new KeywordRecognizer(audioConfig);
+
+            Console.WriteLine("Say \"Hey Roku...\"");
+            var result = await keywordRecognizer.RecognizeOnceAsync(keywordModel);
+            Console.WriteLine(result.Text);
+        }
+    }
+}
